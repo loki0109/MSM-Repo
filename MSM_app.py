85a01fe68d2d0cf52b7d1f99041ad30f092ac2db
Lokesh <Lokesh.M@corestack.io>
2022-09-13 14:57:14 +0530

    completed signup

--- MSM_app.py
+++ MSM_app.py
@@ -1,14 +1,16 @@
+from ast import Pass
 from asyncio.windows_events import NULL
 from flask import Flask,render_template,request,Response,json, flash, redirect, url_for,session
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = "mongodb://localhost/msm_db"
+app.config['MONGO_URI'] = "mongodb://localhost:27017/msm_db"
 mongo = PyMongo(app)
 
 db = mongo.db.users
 
+app.secret_key = "loki-1238sanmxg-dcj1248o"
 @app.route("/signup",methods = ["POST", "GET"])
 def signup():  
     if request.method == "POST":
@@ -16,21 +18,22 @@ def signup():
         Email = request.form['Email']
         User_name = request.form['User_name']
         Password = request.form['Password']
-        hashed_password = Bcrypt.generate_password_hash(Password).decode('utf-8')
-        if db.find({'Email':{"$exists":True, "$ne":NULL}}):
-            flash(f'Email Id already exists')
+        print(Email)
+        # hashed_password = Bcrypt.generate_password_hash(Password).decode('utf-8')
+        if((db.count_documents({'email':Email}))!=0):
+
+            flash(f'Email Id already exists','danger')
         else:
             new_user = {
                 'name': Full_name,
                 'email':Email,
                 'User_name':User_name,
-                'Password': hashed_password
+                'Password': Password
             }
             db.insert_one(new_user)
             print(new_user)
             flash(f'User{Full_name} is successfully created','success')
             return redirect(url_for('login'))
-    else:
         return render_template('signup.html')
     
 @app.route("/login", methods = ["GET"])
